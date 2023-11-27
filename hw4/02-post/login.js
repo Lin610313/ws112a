4e9936a2796272c7a6a82494050b6633a5dc10e3
Lin610313 <79679010+Lin610313@users.noreply.github.com>
2023-11-28 01:52:20 +0800

    Update login.js

---
+++ hw4/02-post/login.js
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="style.css">
+<form action="/people/login" method="post">
+    <input type="text" name="account" placeholder="帳號" required><br>
+    <input type="password" name="password" placeholder="密碼" required><br>
+    <input type="submit" value="login">
+</form>
